d0a4e70ec496a4b283c18b99a5090cfa0c3cb903
FUBARwithall <panjirafi96@gmail.com>
2026-08-01 08:30:50 +0700

    fix: integrate MediaScannerConnection to register downloaded receipts in Android Gallery instantly

--- lib/app/data/utils/download_helper_stub.dart
+++ lib/app/data/utils/download_helper_stub.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 
-void downloadBmpFile(Uint8List bmpBytes, String fileName) {
+const MethodChannel _channel = MethodChannel('com.example.junior_mobile_programmer/media_scanner');
+
+Future<void> downloadBmpFile(Uint8List bmpBytes, String fileName) async {
   try {
     Directory saveDir = Directory.systemTemp;
     if (Platform.isAndroid) {
@@ -18,6 +21,15 @@ void downloadBmpFile(Uint8List bmpBytes, String fileName) {
     final file = File('${saveDir.path}/$fileName');
     file.writeAsBytesSync(bmpBytes);
     debugPrint('[DownloadHelper] BMP saved to public path: ${file.path}');
+
+    if (Platform.isAndroid) {
+      try {
+        await _channel.invokeMethod('scanFile', {'path': file.path});
+        debugPrint('[DownloadHelper] MediaScanner notified for ${file.path}');
+      } catch (e) {
+        debugPrint('[DownloadHelper] MediaScanner error: $e');
+      }
+    }
   } catch (e) {
     debugPrint('[DownloadHelper] Error saving BMP: $e');
   }
